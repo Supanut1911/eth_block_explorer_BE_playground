a22fa58e3255a9869d410fb434407a77a803942f
Supanut1911 <ynotmela@gmail.com>
2023-11-10 14:56:45 +0700

    add dotenv

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
-// src/app.ts
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import "dotenv/config";
 
-const app: Express = express();
+console.log(process.env.YOYO);
 
+const app = express();
 const port: number = 3000;
 
+// Add a variable for the api key, address, and chain
+const MORALIS_API_KEY: string = "replace_me";
+const address: string = "replace_me";
+const chain: EvmChain = EvmChain.ETHEREUM;
+
 app.get("/", (req: Request, res: Response) => {
-  res.json({
-    message: "Hello Express + TypeScirpt!!",
-  });
+  res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Application is running on port ${port}`));
+// Add this a startServer function that initializes Moralis
+const startServer = async (): Promise<void> => {
+  await Moralis.start({
+    apiKey: "xxx",
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+// Call startServer()
+startServer();
