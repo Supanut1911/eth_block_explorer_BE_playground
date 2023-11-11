0a61573f6abc2ce8c8182e39535ba553c61f4024
Supanut1911 <ynotmela@gmail.com>
2023-11-11 15:01:42 +0700

    get eth price

--- index.ts
+++ index.ts
@@ -2,25 +2,39 @@ import express, { Request, Response } from "express";
 import Moralis from "moralis";
 import { EvmChain } from "@moralisweb3/common-evm-utils";
 import "dotenv/config";
-
-console.log(process.env.YOYO);
-
+import { log } from "console";
+// import * as cors from "cors";
+const cors = require("cors");
 const app = express();
-const port: number = 3000;
+
+// app.use(cors);
+const port: number = 3001;
 
 // Add a variable for the api key, address, and chain
-const MORALIS_API_KEY: string = "replace_me";
-const address: string = "replace_me";
+const MORALIS_API_KEY: string = `${process.env.MORALIS_API_KEY}`;
+const address: string = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 const chain: EvmChain = EvmChain.ETHEREUM;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.json({ message: "yoyo" });
+});
+
+app.get("/getETHprice", async (req: Request, res: Response) => {
+  try {
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      address,
+      chain,
+    });
+    res.status(200).json({ response });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 // Add this a startServer function that initializes Moralis
 const startServer = async (): Promise<void> => {
-  await Moralis.start({
-    apiKey: "xxx",
+  const x = await Moralis.start({
+    apiKey: MORALIS_API_KEY,
   });
 
   app.listen(port, () => {
